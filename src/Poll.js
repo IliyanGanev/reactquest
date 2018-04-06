6c8781372b901f2344fd75ced13b0c8abb1c20d9
Iliyan Ganev <ganeviliyan@gmail.com>
2018-04-05 22:32:50 -0700

    Fix functionality, add some style

--- src/Poll.js
+++ src/Poll.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './Poll.css';
 
 class Poll extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
 
       selectedOption: '',
+      isSelected: false,
+      id: 1,
 
       questions:[{
         "id": 1,
@@ -45,84 +46,105 @@ class Poll extends Component {
       ]
     };
     this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
 
 
   handleOptionChange(changeEvent) {
-    console.log(changeEvent.target.value, this)
-    var result=changeEvent.target.value
+    changeEvent.preventDefault();
     this.setState({
-      selectedOption: changeEvent.target.value
+      selectedOption: changeEvent.target.value,
+      isSelected: true,
+      id: changeEvent.target.id
     });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      selectedOption: '',
+      isSelected: false
+    });   
+  }
 
 
   render() {
 
-    var questions = this.state.questions.map((question) =>
+    const isSelected = this.state.isSelected;
 
+    const questions = this.state.questions.map((question) =>
       <li key={question.id}>
       <h3>{question.text}</h3>  
       </li>
       );
 
-    var index = Math.floor(Math.random() * questions.length)
-    var randomQuestion = questions[index];
+    const index = Math.floor(Math.random() * questions.length)
+    const randomQuestion = questions[index];
 
-    var question = this.state.questions.find((question) =>
+
+    const question = this.state.questions.find((question) =>
      question.id === index+1
      );
 
+    const answers = question.answers
 
-    var answers = question.answers
-    var responses = answers.map((a) =>
+    const currentQuestion = this.state.questions.find((question) =>
+      question.id === Number(this.state.id)
+    );
+    const currentAnswers = currentQuestion.answers
+
+    const currentResponses = currentAnswers.map((a) =>
       <li key={a.id}>
-      <h3>{a.text}</h3>  
+      <h2>{a.text}: {a.responses} responses</h2> 
       </li>
     );
 
 
+    const details = isSelected ? (
+      <div>
+        <h4>You selected: <span className={(this.state.selectedOption).toLowerCase()}>{this.state.selectedOption}</span></h4>
+        <ul>{currentResponses}</ul>
+      </div>
+    ) : (<p>Choose your {question.text} !</p>);
+
+
+
     return (
       <div className="Poll">
-      <div className="Poll-header">
-        <h2>World</h2>
-
+        <div className="Poll-header">
+          <h2 className="helloworld">Hello {this.props.name}</h2>
+          {details}
+        </div>
+        
         <ul>{randomQuestion}</ul>
-        <p>{console.log(answers[0].text)}</p>
 
-      </div>
-
-        <ul>{responses}</ul>
-
-
-
-      <form>
-        <div className="radio">
-          <label>
-            <input type="radio" value={answers[0].text}
-            checked={this.state.selectedOption === answers[0].text}
-            onChange={this.handleOptionChange}  /><span>{answers[0].text}</span>
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input type="radio" value={answers[1].text}
-            checked={this.state.selectedOption === answers[1].text}
-            onChange={this.handleOptionChange}  /><span>{answers[1].text}</span>
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input type="radio" value={answers[2].text}
-            checked={this.state.selectedOption === answers[2].text}
-            onChange={this.handleOptionChange}  /><span>{answers[2].text}</span>
-          </label>
-        </div>
-      </form>    
-
-
+        <form onSubmit={this.handleSubmit}>
+          <div className="radio">
+            <label>
+              <input type="radio" value={answers[0].text}
+                id={question.id}
+                checked={this.state.selectedOption === answers[0].text}
+                onChange={this.handleOptionChange}  /><span>{answers[0].text}</span>
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value={answers[1].text}  
+                id={question.id}     
+                checked={this.state.selectedOption === answers[1].text}
+                onChange={this.handleOptionChange}  /><span>{answers[1].text}</span>
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value={answers[2].text}  
+                id={question.id}       
+                checked={this.state.selectedOption === answers[2].text}
+                onChange={this.handleOptionChange}  /><span>{answers[2].text}</span>
+            </label>
+          </div>
+          <button type="submit">Clear selection</button>
+        </form>    
       </div>
     );
   }
